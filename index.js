87bfd1d180632909a38ddb03f91db8bd5ec658ac
pejuameenat <pejuameenat@gmail.com>
2025-03-14 13:16:00 +0100

    fixed game over bug

--- index.js
+++ index.js
@@ -1,74 +1,76 @@
-const cells = document.querySelectorAll('.cell')
-const imageCells = document.querySelectorAll('.img-cell')
-const startBtn = document.querySelector('.start')
-const restartBtn = document.querySelector('.restart')
-const statusText = document.querySelector('.status-text')
-const timeText = document.querySelector('.time')
-const gameOver = document.querySelector('.loser')
-console.log(gameOver)
+const cells = document.querySelectorAll(".cell");
+const imageCells = document.querySelectorAll(".img-cell");
+const startBtn = document.querySelector(".start");
+const restartBtn = document.querySelector(".restart");
+const statusText = document.querySelector(".status-text");
+const timeText = document.querySelector(".time");
+const gameOver = document.querySelector(".loser");
+console.log(gameOver);
 const imgArr = [
-  './images/arrow-down.png',
-  './images/arrow-up.png',
-  './images/here.png',
-  './images/left.png',
-  './images/right.png',
-  './images/up-1.png',
-  './images/up.png',
-]
+  "./images/arrow-down.png",
+  "./images/arrow-up.png",
+  "./images/here.png",
+  "./images/left.png",
+  "./images/right.png",
+  "./images/up-1.png",
+  "./images/up.png",
+];
 
 //Adding opacity to the cells
-let start
+let start;
 function addOpacity() {
-  imageCells.forEach((img) => img.classList.add('opacity'))
+  imageCells.forEach((img) => img.classList.add("opacity"));
 }
 // timer count down
-let timer = 10
+let timer = 10;
+let timerFunc;
 function countDown() {
-  const timerFuc = setInterval(function () {
-    timer--
-    timeText.textContent = '00:' + timer
+  timerFunc = setInterval(function () {
+    timer--;
+    timeText.textContent = "00:" + timer;
     if (timer < 1) {
-      timer = 0
-      clearInterval(timerFuc)
-      cells.forEach((cell) => cell.removeEventListener('click', cellClicked))
-      gameOver.classList.remove('hide')
+      timer = 0;
+      clearInterval(timerFunc);
+      cells.forEach((cell) => cell.removeEventListener("click", cellClicked));
+      gameOver.textContent = "You were too slow! Game Over!";
+      // gameOver.classList.remove('hide')
     }
-  }, 1000)
+  }, 1000);
 }
 //Adding backgrounds
 function cellBackgrounds() {
-  start = true
-  const randomIndex = Math.floor(Math.random() * imgArr.length)
-  const randomImage = imgArr[randomIndex]
+  start = true;
+  const randomIndex = Math.floor(Math.random() * imgArr.length);
+  const randomImage = imgArr[randomIndex];
   for (const img of imageCells) {
-    img.setAttribute('src', randomImage)
+    img.setAttribute("src", randomImage);
   }
 
-  const removedImgArr = []
-  const imgLength = 8
+  const removedImgArr = [];
+  const imgLength = 8;
   for (let i = 0; i < imgLength; i++) {
-    const randomNum = Math.floor(Math.random() * imageCells.length)
-    const removedImageIndex = imageCells[randomNum]
+    const randomNum = Math.floor(Math.random() * imageCells.length);
+    const removedImageIndex = imageCells[randomNum];
     for (const img of removedImgArr) {
-      img.removeAttribute('src')
+      img.removeAttribute("src");
     }
-    removedImgArr.push(removedImageIndex)
+    removedImgArr.push(removedImageIndex);
   }
-   setTimeout(addOpacity, 2000)
-   //only start the timer when the opacity has been added
-  if(start)countDown()
-  cells.forEach((cell) => cell.addEventListener('click', cellClicked))
+  setTimeout(() => {
+    addOpacity();
+    countDown(); // Start timer only after opacity is applied
+    cells.forEach((cell) => cell.addEventListener("click", cellClicked));
+  }, 2000);
 }
+startBtn.addEventListener("click", cellBackgrounds);
 
-startBtn.addEventListener('click', cellBackgrounds)
-
-const correctCells = []
+const correctCells = [];
 function cellClicked() {
-  const cellIndex = this.getAttribute('cellIndex')
-  if (imageCells[cellIndex].src !== '') {
-    this.style.backgroundColor = '#0f0'
-    correctCells.push(imageCells[cellIndex])
-    const combinedCellArr = [...correctCells]
+  const cellIndex = this.getAttribute("cellIndex");
+  if (imageCells[cellIndex].src !== "") {
+    this.style.backgroundColor = "#0f0";
+    correctCells.push(imageCells[cellIndex]);
+    const combinedCellArr = [...correctCells];
     // if the last element at the correct index is clicked win!
     for (const correctCell of combinedCellArr) {
       if (combinedCellArr.length >= 9) {
@@ -77,25 +79,28 @@ function cellClicked() {
           combinedCellArr.length - 1
         ) {
           for (const cell0 of cells) {
-            cell0.style.backgroundColor = '#0f0'
-            cell0.removeEventListener('click', cellClicked)
-            statusText.textContent = 'Congrats You win!🎉🎊'
+            cell0.style.backgroundColor = "#0f0";
+            cell0.removeEventListener("click", cellClicked);
+            statusText.textContent = "Congrats You win!🎉🎊";
+            clearInterval(timerFunc);
           }
         }
       }
     }
   } else {
-     //did this because the timer still counts at  least one time
-     timer = 1
-   //   if (timer === 0) {
-   //      gameOver.classList.add('hide')
-   //   }
-    for (const cell of cells) cell.style.backgroundColor = '#f00'
-    statusText.textContent = 'Ha! You lost ☹ better luck next time'
-     cells.forEach((cell) => cell.removeEventListener('click', cellClicked))
+    //did this because the timer still counts at  least one time
+    //   timer = 1
+    for (const cell of cells) cell.style.backgroundColor = "#f00";
+    gameOver.textContent = "Ha! You lost ☹ better luck next time";
+    cells.forEach((cell) => cell.removeEventListener("click", cellClicked));
+     clearInterval(timerFunc);
+     //disable the start button
+     startBtn.setAttribute("disabled", "true");
+     console.log(startBtn)
   }
 }
 
-restartBtn.addEventListener('click', function () {
-  window.location.reload()
-})
+restartBtn.addEventListener("click", function () {
+  window.location.reload();
+  startBtn.setAttribute("disabled", "false");
+});
